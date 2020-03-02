763aca27ff3dadb684ce1597aefd6d18c8ad43df
Michael Herman <hermanmu@gmail.com>
2020-03-02 07:22:42 -0500

    add remaining crud routes

--- project/api/users.py
+++ project/api/users.py
@@ -29,6 +29,32 @@ class Users(Resource):
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
 
+    @api.expect(user, validate=True)
+    def put(self, user_id):
+        post_data = request.get_json()
+        username = post_data.get("username")
+        email = post_data.get("email")
+        response_object = {}
+
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        user.username = username
+        user.email = email
+        db.session.commit()
+        response_object["message"] = f"{user.id} was updated!"
+        return response_object, 200
+
+    def delete(self, user_id):
+        response_object = {}
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            api.abort(404, f"User {user_id} does not exist")
+        db.session.delete(user)
+        db.session.commit()
+        response_object["message"] = f"{user.email} was removed!"
+        return response_object, 200
+
 
 class UsersList(Resource):
     @api.marshal_with(user, as_list=True)
