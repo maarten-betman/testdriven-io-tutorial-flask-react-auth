36b1bbf2f8e76d7426f88b4c8c5e0a3b033f7771
Michael Herman <hermanmu@gmail.com>
2020-03-01 20:29:43 -0500

    add tests

--- project/__init__.py
+++ project/__init__.py
@@ -1,25 +1,39 @@
 # project/__init__.py
 
 
-from flask import Flask, jsonify
-from flask_restx import Resource, Api
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
-# instantiate the app
-app = Flask(__name__)
-
-api = Api(app)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')
+# instantiate the db
+db = SQLAlchemy()
 
 
-class Ping(Resource):
-    def get(self):
-        return {
-        'status': 'success',
-        'message': 'pong!'
-    }
+def create_app(script_info=None):
 
+    # instantiate the app
+    app = Flask(__name__)
 
-api.add_resource(Ping, '/ping')
+    # set config
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.ping import ping_blueprint
+
+    app.register_blueprint(ping_blueprint)
+    from project.api.users import users_blueprint
+
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
