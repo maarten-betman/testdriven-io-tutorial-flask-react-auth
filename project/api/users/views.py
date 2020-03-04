7d2af638e40d2188db50257ef5ab14d1392e917b
Michael Herman <hermanmu@gmail.com>
2020-03-04 14:45:17 -0500

    add swagger

--- project/api/users/views.py
+++ project/api/users/views.py
@@ -1,22 +1,22 @@
 # project/api/users/views.py
 
 
-from flask import Blueprint, request
-from flask_restx import Api, Resource, fields
+from flask import request
+from flask_restx import Resource, fields, Namespace
 
-from project.api.users.crud import (  # noqa isort:skip
-    add_user,
-    delete_user,
+from project.api.users.crud import (
     get_all_users,
     get_user_by_email,
+    add_user,
     get_user_by_id,
     update_user,
+    delete_user,
 )
 
-users_blueprint = Blueprint("users", __name__)
-api = Api(users_blueprint)
 
-user = api.model(
+users_namespace = Namespace("users")
+
+user = users_namespace.model(
     "User",
     {
         "id": fields.Integer(readOnly=True),
@@ -28,12 +28,16 @@ user = api.model(
 
 
 class UsersList(Resource):
-    @api.marshal_with(user, as_list=True)
+    @users_namespace.marshal_with(user, as_list=True)
     def get(self):
+        """Returns all users."""
         return get_all_users(), 200
 
-    @api.expect(user, validate=True)
+    @users_namespace.expect(user, validate=True)
+    @users_namespace.response(201, "<user_email> was added!")
+    @users_namespace.response(400, "Sorry. That email already exists.")
     def post(self):
+        """Creates a new user."""
         post_data = request.get_json()
         username = post_data.get("username")
         email = post_data.get("email")
@@ -49,15 +53,21 @@ class UsersList(Resource):
 
 
 class Users(Resource):
-    @api.marshal_with(user)
+    @users_namespace.marshal_with(user)
+    @users_namespace.response(200, "Success")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def get(self, user_id):
+        """Returns a single user."""
         user = get_user_by_id(user_id)
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
         return user, 200
 
-    @api.expect(user, validate=True)
+    @users_namespace.expect(user, validate=True)
+    @users_namespace.response(200, "<user_is> was updated!")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def put(self, user_id):
+        """Updates a user."""
         post_data = request.get_json()
         username = post_data.get("username")
         email = post_data.get("email")
@@ -65,20 +75,23 @@ class Users(Resource):
 
         user = get_user_by_id(user_id)
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
         update_user(user, username, email)
         response_object["message"] = f"{user.id} was updated!"
         return response_object, 200
 
+    @users_namespace.response(200, "<user_is> was removed!")
+    @users_namespace.response(404, "User <user_id> does not exist")
     def delete(self, user_id):
+        """Updates a user."""
         response_object = {}
         user = get_user_by_id(user_id)
         if not user:
-            api.abort(404, f"User {user_id} does not exist")
+            users_namespace.abort(404, f"User {user_id} does not exist")
         delete_user(user)
         response_object["message"] = f"{user.email} was removed!"
         return response_object, 200
 
 
-api.add_resource(UsersList, "/users")
-api.add_resource(Users, "/users/<int:user_id>")
+users_namespace.add_resource(UsersList, "")
+users_namespace.add_resource(Users, "/<int:user_id>")
